e09229e6468887a6db31754d283f820fe3a3c5a5
GabrielGil-Castro <gsgc05@gmail.com>
2023-03-14 19:18:10 -0300

    requisito 5 completo

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,12 +1,45 @@
 import { Component } from 'react';
 import Header from '../components/Header';
 
+const minLength = 2;
+
 class Search extends Component {
+  state = {
+    artist: '',
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { artist } = this.state;
+    const isValid = artist.length >= minLength;
+
     return (
       <div data-testid="page-search">
         <Header />
-        <p>Search</p>
+        <form>
+          <label htmlFor="artist">
+            <input
+              type="text"
+              name="artist"
+              id="artist"
+              value={ artist }
+              data-testid="search-artist-input"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            data-testid="search-artist-button"
+            disabled={ !isValid }
+          >
+            Pesquisar
+          </button>
+
+        </form>
       </div>
     );
   }
