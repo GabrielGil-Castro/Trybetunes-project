1016002bf3813863f24953c1f28682f8fc19a86d
GabrielGil-Castro <gsgc05@gmail.com>
2023-03-21 18:05:16 -0300

    requisito 9

--- src/pages/MusicCard.js
+++ src/pages/MusicCard.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Loading from './Loading';
-import { addSong } from '../services/favoriteSongsAPI';
+import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 export default class MusicCard extends Component {
   state = {
     loading: false,
+    favoriteList: [],
   };
+
+  async componentDidMount() {
+    this.setState({
+      loading: true,
+    });
+    const favoriteSong = await getFavoriteSongs();
+    this.setState({
+      favoriteList: favoriteSong,
+      loading: false,
+    });
+  }
 
   handleChangeFavorite = async (songObj) => {
     this.setState({
@@ -20,7 +32,7 @@ export default class MusicCard extends Component {
 
   render() {
     const { music } = this.props;
-    const { loading } = this.state;
+    const { loading, favoriteList } = this.state;
     return (
       <div>
         {loading && <Loading />}
@@ -44,6 +56,9 @@ export default class MusicCard extends Component {
                   type="checkbox"
                   data-testid={ `checkbox-music-${song.trackId}` }
                   onChange={ () => this.handleChangeFavorite(song) }
+                  checked={
+                    favoriteList.find((favorite) => favorite.trackId === song.trackId)
+                  }
                 />
               </label>
             </div>))
@@ -54,5 +69,38 @@ export default class MusicCard extends Component {
 }
 
 MusicCard.propTypes = {
-  music: PropTypes.arrayOf(PropTypes.object).isRequired,
+  music: PropTypes.arrayOf(PropTypes.shape({
+    artistId: PropTypes.number.isRequired,
+    artistName: PropTypes.string.isRequired,
+    artistViewUrl: PropTypes.string.isRequired,
+    artworkUrl30: PropTypes.string.isRequired,
+    artworkUrl60: PropTypes.string.isRequired,
+    artworkUrl100: PropTypes.string.isRequired,
+    collectionCensoredName: PropTypes.string.isRequired,
+    collectionExplicitness: PropTypes.string.isRequired,
+    collectionId: PropTypes.number.isRequired,
+    collectionName: PropTypes.string.isRequired,
+    collectionPrice: PropTypes.number.isRequired,
+    collectionViewUrl: PropTypes.string.isRequired,
+    country: PropTypes.string.isRequired,
+    currency: PropTypes.string.isRequired,
+    discCount: PropTypes.number.isRequired,
+    discNumber: PropTypes.number.isRequired,
+    isStreamable: PropTypes.bool.isRequired,
+    kind: PropTypes.string.isRequired,
+    previewUrl: PropTypes.string.isRequired,
+    primaryGenreName: PropTypes.string.isRequired,
+    releaseDate: PropTypes.string.isRequired,
+    trackCensoredName: PropTypes.string.isRequired,
+    trackCount: PropTypes.number.isRequired,
+    trackExplicitness: PropTypes.string.isRequired,
+    trackId: PropTypes.number.isRequired,
+    trackName: PropTypes.string.isRequired,
+    trackNumber: PropTypes.number.isRequired,
+    trackPrice: PropTypes.number.isRequired,
+    trackTimeMillis: PropTypes.number.isRequired,
+    trackViewUrl: PropTypes.string.isRequired,
+    wrapperType: PropTypes.string.isRequired,
+  }).isRequired)
+    .isRequired,
 };
